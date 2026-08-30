21da341794bc0574968d9e6411f3e6bfc0587962
Cameron Anderson <cammando@hotmail.com.au>
2026-08-30 18:15:36 +1000

    Update home page layout to render CodeFile components for content from array of file data rather than manually

--- 2026-portfolio/app/page.tsx
+++ 2026-portfolio/app/page.tsx
@@ -1,8 +1,22 @@
+'use client'
+
 import CodeFile from '../components/CodeFile';
+import { useState } from 'react';
 import styles from '../styles/home-layout.module.scss';
 import globalStyles from '../styles/global.module.scss';
 
 export default function Home() {
+  const [fileData, setFileData] = useState<{key : string; screenQuarter: Number, fileName: string, lineCount: number, showFile: boolean }[]> ([
+    { key: crypto.randomUUID(), screenQuarter: 1, fileName: 'About Me', lineCount: 20, showFile: true },
+    { key: crypto.randomUUID(), screenQuarter: 2, fileName: 'Github Graph', lineCount: 6, showFile: true },
+    { key: crypto.randomUUID(), screenQuarter: 3, fileName: 'Github Graph', lineCount: 6, showFile: true },
+    { key: crypto.randomUUID(), screenQuarter: 4, fileName: 'Contact', lineCount: 15, showFile: true },
+  ])
+
+  const quarter1Files = fileData.filter(f => f.screenQuarter === 1 && f.showFile);
+  const quarter2Files = fileData.filter(f => f.screenQuarter === 2 && f.showFile);
+  const quarter3Files = fileData.filter(f => f.screenQuarter === 3 && f.showFile);
+  const quarter4Files = fileData.filter(f => f.screenQuarter === 4 && f.showFile);
   return (
     <div className={styles.window}>
       <div className={`${styles.topScreenBar} ${styles.rowFlex} `}>
@@ -20,12 +34,32 @@ export default function Home() {
         </div>
 
         <div className={`${styles.rowFlex} ${styles.contentPane}`}>
-          <CodeFile fileName='About Me' lineCount={10} />
-
-          <div className={`${styles.splitContentPane}`}>
-            <CodeFile fileName='Github Graph' lineCount={4} />
-            <CodeFile fileName='Contact' lineCount={20}/>
-          </div>
+          {
+            (quarter1Files.length > 0 || quarter2Files.length > 0) &&
+            (
+              <div className={`${styles.verticalFileSplit}`}>
+                {
+                  quarter1Files.map(f => <CodeFile fileName={f.fileName} lineCount={f.lineCount} key={f.key} />)
+                }
+                {
+                  quarter2Files.map(f => <CodeFile fileName={f.fileName} lineCount={f.lineCount} key={f.key} />)
+                }
+              </div>
+            )
+          }
+          {
+            (quarter3Files.length > 0 || quarter4Files.length > 0) &&
+            (
+              <div className={`${styles.verticalFileSplit}`}>
+                {
+                  quarter3Files.map(f => <CodeFile fileName={f.fileName} lineCount={f.lineCount} key={f.key} />)
+                }
+                {
+                  quarter4Files.map(f => <CodeFile fileName={f.fileName} lineCount={f.lineCount} key={f.key} />)
+                }
+              </div>
+            )
+          }
         </div>
       </div>
 
