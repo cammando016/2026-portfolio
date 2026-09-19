b866bb1dcc489d960dcd3d2701bc9f840c389d5b
Cameron Anderson <cammando@hotmail.com.au>
2026-09-19 13:26:08 +1000

    Add project screenshots

--- 2026-portfolio/data/projectsConfig.ts
+++ 2026-portfolio/data/projectsConfig.ts
@@ -1,6 +1,20 @@
 import { FileData } from "../types/Files";
 import nbaScreenshot0 from '../assets/nbaScreenshot0.png';
 import nbaScreenshot1 from '../assets/nbaScreenshot1.png';
+import nbaScreenshot2 from '../assets/nbaHomeMobile.png';
+import nbaScreenshot3 from '../assets/nbaGameplayMobile.png';
+import nbaScreenshot4 from '../assets/nbaGameoverMobile.png';
+import calendarScreenshot0 from '../assets/calendarSignUp.png';
+import calendarScreenshot1 from '../assets/calendarLogin.png';
+import calendarScreenshot2 from '../assets/calendarMonthView.png';
+import calendarScreenshot3 from '../assets/calendarYearView.png';
+import calendarScreenshot4 from '../assets/calendarEditAccount.png';
+import calendarScreenshot5 from '../assets/calendarAccountRecovery.png';
+import calendarScreenshot6 from '../assets/calendarEventList.png';
+import calendarScreenshot7 from '../assets/calendarCreateEvent.png';
+import calendarScreenshot8 from '../assets/calendarGroupList.png';
+import calendarScreenshot9 from '../assets/calendarEditGroup.png';
+import resumeScreenshot0 from '../assets/resumeScreenshot0.png';
 
 export interface ProjectConfig {
     slug: string,
@@ -18,7 +32,27 @@ export const projectsConfig: ProjectConfig[] = [
         title: 'Resume',
         githubRepo: { owner: 'cammando016', repo: 'resume-draft'},
         staticFileData: [
-            
+            {
+                key: crypto.randomUUID(),
+                screenQuarter: 3,
+                fileName: 'Project Links',
+                lineCount: 2,
+                fileOpen: true,
+                activeFileInQuarter: true,
+                contentComponent: 'projectLinks',
+                projectLink: 'https://c-anderson-resume.netlify.app/#education-section',
+                githubLink: 'https://github.com/cammando016/resume-draft'
+            },
+            {
+                key: crypto.randomUUID(),
+                screenQuarter: 4,
+                fileName: 'Project Screenshots',
+                lineCount: 0,
+                fileOpen: true,
+                activeFileInQuarter: true,
+                contentComponent: 'screenshots',
+                screenshots: [resumeScreenshot0]
+            }
         ],
     },
     {
@@ -45,7 +79,7 @@ export const projectsConfig: ProjectConfig[] = [
                 fileOpen: true,
                 activeFileInQuarter: true,
                 contentComponent: 'screenshots',
-                screenshots: [nbaScreenshot0, nbaScreenshot1]
+                screenshots: [nbaScreenshot0, nbaScreenshot1, nbaScreenshot2, nbaScreenshot3, nbaScreenshot4]
             }
         ],
     },
@@ -54,7 +88,27 @@ export const projectsConfig: ProjectConfig[] = [
         title: 'Group Calendar',
         githubRepo: { owner: 'cammando016', repo: 'calendar'},
         staticFileData: [
-
+            {
+                key: crypto.randomUUID(),
+                screenQuarter: 3,
+                fileName: 'Project Links',
+                lineCount: 2,
+                fileOpen: true,
+                activeFileInQuarter: true,
+                contentComponent: 'projectLinks',
+                projectLink: 'https://calendar-1-xauw.onrender.com',
+                githubLink: 'https://github.com/cammando016/calendar'
+            },
+            {
+                key: crypto.randomUUID(),
+                screenQuarter: 4,
+                fileName: 'Project Screenshots',
+                lineCount: 0,
+                fileOpen: true,
+                activeFileInQuarter: true,
+                contentComponent: 'screenshots',
+                screenshots: [calendarScreenshot0, calendarScreenshot1, calendarScreenshot2, calendarScreenshot3, calendarScreenshot4, calendarScreenshot5, calendarScreenshot6, calendarScreenshot7, calendarScreenshot8, calendarScreenshot9 ]
+            }
         ],
     },
     {
@@ -62,7 +116,16 @@ export const projectsConfig: ProjectConfig[] = [
         title: 'Gym Tracker',
         githubRepo: { owner: 'cammando016', repo: 'gym'},
         staticFileData: [
-
+            {
+                key: crypto.randomUUID(),
+                screenQuarter: 3,
+                fileName: 'Project Links',
+                lineCount: 2,
+                fileOpen: true,
+                activeFileInQuarter: true,
+                contentComponent: 'projectLinks',
+                githubLink: 'https://github.com/cammando016/gym'
+            },
         ],
     },
 ]
