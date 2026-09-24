1df513e03e886e662a3f1133e20b0f581d13753d
Cameron Anderson <cammando@hotmail.com.au>
2026-09-24 15:02:08 +1000

    Different message displayed on landing page on mobile vs desktop

--- 2026-portfolio/components/contentComponents/Landing.tsx
+++ 2026-portfolio/components/contentComponents/Landing.tsx
@@ -10,8 +10,9 @@ export default function Landing () {
             <ul>
                 <li>Use the files icon to navigate between pages</li>
                 <li>You can open and close the files within each file folder</li>
-                <li>Click on any of my project links to fetch the README from my github repo and see project screenshots.</li>
-                <li>Move files around to change the display layout by dragging and dropping around the screen as you would on VS Code</li>
+                <li>Click on any of my project links to fetch the README from my github repo and see project screenshots</li>
+                <li className={`${contentStyles.landingDesktop}`}>Move files around to change the display layout by dragging and dropping around the screen as you would on VS Code</li>
+                <li className={`${contentStyles.landingMobile}`}>Try visiting on a larger screen to move files around multiple displayed file windows on screen</li>
                 <li>Customise the page to your preferences from the settings tab</li>
             </ul>
             <br />
