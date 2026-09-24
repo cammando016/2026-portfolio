f58cd3d141f0450822640e6c596272f2ebb36a09
Cameron Anderson <cammando@hotmail.com.au>
2026-09-24 22:48:09 +1000

    Update Settings page to add setting for displaying element tags next to text

--- 2026-portfolio/components/Settings.tsx
+++ 2026-portfolio/components/Settings.tsx
@@ -1,17 +1,21 @@
 'use client'
 
 import { useThemeContext } from "../store/colourSchemeContext";
+import { useShowTagsContext } from "../store/showTagsContext";
 import styles from '../styles/settings.module.scss';
+import codeFileStyles from '../styles/codeFile.module.scss';
 import { COLOUR_SCHEMES } from "../types/Files";
 
 export default function Settings() {
     const { theme, setTheme } = useThemeContext();
+    const { showTags, toggleShowTags } = useShowTagsContext();
 
     const handleChangeTheme = (e: any) => setTheme(e.target.value)
 
     return (
         <div className={`${styles.settingsContainer}`}>
-            <h3>Settings</h3>
+            <h1>Settings</h1>
+            <br />
             <fieldset className={`${styles.themeFieldset}`}>
                 <legend className={`${styles.themeLegend}`}>Update Colour Theme</legend>
                 {COLOUR_SCHEMES.map(c => {
@@ -30,6 +34,36 @@ export default function Settings() {
                     )
                 })
                 }
+            </fieldset>
+            <br />
+            <fieldset className={styles.themeFieldset}>
+                <legend className={styles.themeLegend}>Show Element Tags?</legend>
+                <label className={styles.label}>
+                    <input 
+                        type="radio"
+                        name="on"
+                        value="true"
+                        checked={showTags}
+                        onChange={toggleShowTags}
+                        className={styles.radioSelector}
+                    />
+                    <span className={styles.span}>
+                        <span className={codeFileStyles.tagLabel}>{`<span>`}</span>
+                        Element Tags On
+                        <span className={codeFileStyles.tagLabel}>{`<span>`}</span>
+                    </span>
+                </label>
+                <label className={styles.label}>
+                    <input 
+                        type="radio"
+                        name="off"
+                        value="false"
+                        checked={!showTags}
+                        onChange={toggleShowTags}
+                        className={styles.radioSelector}
+                    />
+                    <span className={styles.span}>Element Tags Off</span>
+                </label>
             </fieldset>
         </div>
     )
