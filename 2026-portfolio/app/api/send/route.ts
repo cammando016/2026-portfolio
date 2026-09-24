653a99b11fc77a6b9c03efc49c49827a84cbfd24
Cameron Anderson <cammando@hotmail.com.au>
2026-09-24 10:38:37 +1000

    Install resend package

---
+++ 2026-portfolio/app/api/send/route.ts
@@ -0,0 +1,9 @@
+import EmailTemplate from "../../../components/EmailTemplate";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+    try {
+    } catch {}
+}
